e75c1bce0f859baeaea7baec73b81a239c27fbc4
John McLoskey <john@MacBook-Air.local>
2020-07-26 14:03:06 -0500

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Bible msg="Welcome to Bible App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import Chapter from "@/components/Chapter.vue";
+// import Book from "@/components/Book.vue";
+import Bible from "@/components/Bible.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    // Chapter,
+    // Book,
+    Bible
   }
 };
 </script>
---
+++ src/views/Read.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>
+    <span :key="verse.id" v-for="verse in kjv">{{ verse.verse_text }}</span>
+  </p>
+</template>
+
+<script>
+import kjv from "@/json/kjv.json";
+export default {
+  name: "Read",
+  data() {
+    return {
+      kjv: kjv
+    };
+  },
+  mounted() {
+    this.kjv.forEach(verse => console.log(verse));
+  }
+};
+</script>
